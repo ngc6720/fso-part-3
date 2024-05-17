99b349747bd51b072524866cd003b71f60370873
ngc6720 <francoiskerforn@gmail.com>
2024-05-17 12:18:50 +0200

    ex 3.22

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+export default [
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { ignores: ["dist/"] },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  {
+    rules: {
+      eqeqeq: "error",
+      "no-unused-vars": ["warn", { vars: "all", args: "none" }],
+      "arrow-spacing": ["warn", { before: true, after: true }],
+      "object-curly-spacing": ["warn", "always"],
+      "no-trailing-spaces": "warn",
+    },
+  },
+];
